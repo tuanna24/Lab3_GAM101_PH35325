9e9bfd80050f55c9ba8844e5fcb0006cf207ca9e
tuanna24 <nguyenanhtuan2402aa@gmail.com>
2024-01-13 12:05:55 +0700

    update

--- Assets/diChuyen.cs
+++ Assets/diChuyen.cs
@@ -9,26 +9,61 @@ public class diChuyen : MonoBehaviour
     public int tocDo = 4;
     public float traiPhai;
     public bool isFacingRight = true;
-    
-    void Start()
+
+    private bool isJumping = false;
+    private int jumpCount = 0;
+    public int maxJumpCount = 2;
+    public float jumpForce = 10f;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Debug.Log("Trigger vào: " + other.gameObject.tag);
+
+        if (other.gameObject.tag == "coin")
+        {
+            Destroy(other.gameObject);
+        }
     }
 
-    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Va chạm vào: " + other.gameObject.tag);
+    }
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
         traiPhai = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(tocDo * traiPhai, rb.velocity.y);
 
-         if (isFacingRight == true && traiPhai == -1) {
+        if (isFacingRight == true && traiPhai == -1)
+        {
             transform.localScale = new Vector3(-1, 1, 1);
             isFacingRight = false;
         }
-        if (isFacingRight == false && traiPhai == 1) {
+        if (isFacingRight == false && traiPhai == 1)
+        {
             transform.localScale = new Vector3(1, 1, 1);
             isFacingRight = true;
         }
-        anim.SetFloat("DiChuyen",Mathf.Abs(traiPhai));
+
+        anim.SetFloat("DiChuyen", Mathf.Abs(traiPhai));
+
+        if (Input.GetButtonDown("Jump") && (jumpCount < maxJumpCount || rb.velocity.y == 0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+            jumpCount++;
+        }
+
+        if (rb.velocity.y == 0 && isJumping)
+        {
+            isJumping = false;
+            jumpCount = 0;
+        }
     }
 }
